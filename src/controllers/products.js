46117b24ef0dc43ed40aa8571a4e17f72c28f386
Roman Soltan <romabandit88@gmail.com>
2025-04-08 19:31:38 +0300

    task-4

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,4 +1,5 @@
-import { getAllProducts } from '../services/products.js';
+import createHttpError from 'http-errors';
+import { getAllProducts, getProductById } from '../services/products.js';
 
 export const getAllProductsController = async (req, res) => {
   const data = await getAllProducts();
@@ -6,6 +7,21 @@ export const getAllProductsController = async (req, res) => {
   res.json({
     status: 200,
     message: 'Successfully found products!',
+    data,
+  });
+};
+
+export const getProductByIdConstroller = async (req, res) => {
+  const { id } = req.params;
+  const data = await getProductById(id);
+
+  if (!data) {
+    throw createHttpError(404, 'Product not found');
+  }
+
+  res.json({
+    status: 200,
+    message: `Successfully found product with id ${id}!`,
     data,
   });
 };
--- src/routers/products.js
+++ src/routers/products.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-import { getAllProductsController } from '../controllers/products.js';
+import {
+  getAllProductsController,
+  getProductByIdConstroller,
+} from '../controllers/products.js';
 
 const productsRouter = Router();
 
 productsRouter.get('/', ctrlWrapper(getAllProductsController));
+
+productsRouter.get('/:id', ctrlWrapper(getProductByIdConstroller));
 
 export default productsRouter;
